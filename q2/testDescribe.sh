f60ed50355151f97952449d96e914e814a64e91e
Angad Chahal <angadchahal@Angads-MacBook-Air.local>
2026-02-04 13:47:50 -0700

    q2 testDescribe test1

---
+++ q2/testDescribe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+args=$#
+
+if [ ${args} -eq 0 ]; then
+    echo "Usage:No arguments were given"
+    exit 1 
+fi 
+
+for stem in $(cat $1); do  
+    echo "Description for the test case ${stem}:"
+    if [ ! -e "${stem}.desc" ]; then
+        echo "${stem}: No test description\n"
+    fi
+    echo $(cat ${stem}.desc)
+done
+    
